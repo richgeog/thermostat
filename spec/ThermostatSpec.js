a6f69de25051adb4121cf0c4cc5e3b3de16d9c39
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-08-17 16:22:57 +0100

    temperature minimum set

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -22,5 +22,22 @@ describe ("Thermostat", function() {
     });
   });
 
+  describe("temperature decrease", function() {
+
+    it("decreases temperature", function() {
+      thermostat.decreaseTemperature();
+      expect(thermostat.showTemperature()).toEqual(19);
+    });
+  });
+
+  describe("minimum temperature", function() {
+
+    it("can only be decreased to 10 degrees", function() {
+      for (i = 0; i < 11; i++) {
+        thermostat.decreaseTemperature();
+      }
+      expect(thermostat.showTemperature()).toEqual(10);
+    });
+  });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -7,7 +7,12 @@ Thermostat.prototype.showTemperature = function() {
 
 };
 
-Thermostat.prototype.increaseTemperature = function(number) {
+Thermostat.prototype.increaseTemperature = function() {
   this.temperature += 1;
+
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+  this.temperature = (this.temperature === 10) ? 10 : this.temperature - 1
 
 };
